b114e1a7a81c79e05bd9bc865dea26ebe7775b71
Iago-leles <iago.leles@gmail.com>
2022-11-30 16:37:54 -0300

    conexão com o bd

---
+++ CRUD_SQLite/Migrations/20221130193507_criacaoBDSQLitee.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CRUD_SQLite.Migrations
+{
+    public partial class criacaoBDSQLitee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
